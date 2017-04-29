69a260d956754ae81c4ec4724cad3bc38c771fcd
Cody Johnson <codyj@protonmail.com>
2017-04-29 17:45:06 -0400

    Fix errors

--- src/Location.cpp
+++ src/Location.cpp
@@ -16,24 +16,31 @@ Location::Location() {
 Location::Location(double &lat, int &ns, double &lon, int &ew) {
     this->lat = lat;
     this->lon = lon;
+    this->ns = ns;
+    this->ew = ew;
     
     // Make sure N is positive
     if (ns == 0 && lat < 0) {
-        this->lat *= -1;
+        this->lat *= -1.0;
+        this->ns = 0;
     }
     
     // Make sure S is negative
     if (ns == 1 && lat > 0) {
-        this->lat *= -1;
+        this->lat *= -1.0;
+        this->ns = 1;
     }
     
     // Make sure E is postive
     if (ew == 0 && lon < 0) {
-        this->lon *= -1;
+        this->lon *= -1.0;
+        this->ew = 0;
     }
     
     // Make sure W is negative
     if (ew == 1 && lon > 0) {
-        this->lon *= -1;
+        this->lon *= -1.0;
+        this->ew = 1;
     }
+    qDebug() << this->lon;
 }
--- src/Location.h
+++ src/Location.h
@@ -7,6 +7,8 @@
 #ifndef _LOCATION_H_
 #define _LOCATION_H_
 
+#include <QDebug>
+
 class Location {
 public:
     // Constructors
@@ -16,14 +18,20 @@ public:
     // Get'er functions
     double getLat() {return this->lat;}
     double getLon() {return this->lon;}
+    int getNS() {return this->ns;}
+    int getEW() {return this->ew;}
     
     // Set'er functions
     void setLat(double l) {this->lat = l;}
     void setLon(double l) {this->lon = l;}
+    void setNS(int i) {this->ns = i;}
+    void setEW(int i) {this->ew = i;}
     
 private:
-    int lat;
-    int lon;
+    double lat;
+    double lon;
+    int ns;
+    int ew;
 };
 
 #endif // _LOCATION_H_
